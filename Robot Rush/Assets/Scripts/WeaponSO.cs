e006c21f373bf268e5afa2dcf851fb39395baad1
parham <parhamqop@gmail.com>
2025-10-30 00:08:01 +0330

    Add Gun And Walking System

---
+++ Robot Rush/Assets/Scripts/WeaponSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponSO", menuName = "Scriptable Objects/WeaponSO")]
+public class WeaponSO : ScriptableObject
+{
+    public int Damage = 5;
+    public float FireRate = .5f;
+    
+}
